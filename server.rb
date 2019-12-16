582d6271c3555576b7ece00ecc77b5739ac467aa
Jemuzu <Jamesiswhoa@gmail.com>
2019-12-16 15:55:48 -0500

    Initialized staff class on ruby

--- server.rb
+++ server.rb
@@ -2,6 +2,21 @@
 require 'httparty'
 require 'sinatra'
 
+set :port, 2000
+
 get '/' do
   erb :index
 end
+
+class Staff
+  attr_accessor :name, :age, :job_Role, :hobbies, :img
+  def initialize(name, age, job_Role, hobbies, img)
+    @name = name
+    @age = age
+    @job_Role = job_Role
+    @hobbies = hobbies
+    @img = img
+  end
+end
+
+puts Staff.name
